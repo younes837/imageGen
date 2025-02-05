53d921ffd3894bfe979374360c9ec670c2638c8b
ynsors2002 <ynsors2002@gmail.com>
2025-02-05 19:42:29 +0100

    first commitv4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        perf_hooks: false,
+        os: false,
+        fs: false,
+        path: false,
+        pg: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
